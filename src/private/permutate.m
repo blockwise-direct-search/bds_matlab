e9811ee2649070402f5477aa403395705f0ddaf6
Lht97 <lhtian97@gmail.com>
2023-05-28 16:08:08 +0800

    Set debug_flag false for permutate. It works locally, so I guess it may be caused by matlab versions. I will figure it out later.
    Former-commit-id: c1bd027241382fcf9a26619fd06fc9d1fbf424b5

--- src/private/permutate.m
+++ src/private/permutate.m
@@ -1,11 +1,17 @@
 function [block_indices] = permutate(block_indices, options)
 % If strategy is randomized_array, then permutate block_indices.
 
-% Preconditions.
-% Whether variable is an array.
-assert(isnumeric(block_indices) && numel(block_indices) >= 1);
-% Whether each of the elements of variable is a positive integer.
-assert(all(block_indices(:) > 0) && all(mod(block_indices(:), 1) == 0));
+
+% Preconditions: If debug_flag is true, then pre-conditions is operated on
+% input. If input_correctness is false, then assert may let the code crash.
+debug_flag = is_debugging();
+
+if debug_flag
+    % Whether variable is an array.
+    assert(isnumeric(block_indices) && numel(block_indices) >= 1);
+    % Whether each of the elements of variable is a positive integer.
+    assert(all(block_indices(:) > 0) && all(mod(block_indices(:), 1) == 0));
+end
 
 if options.blocks_strategy == "Randomized_array"
     nb = length(block_indices);
@@ -14,11 +20,14 @@ else
     return;
 end
 
-% Postconditions.
-% Whether output is an array.
-assert(isnumeric(block_indices) && numel(block_indices) >= 1);
-% Whether each of the elements of output is a positive integer.
-assert(all(block_indices(:) > 0) && all(mod(block_indices(:), 1) == 0));
+% Postconditions: If debug_flag is true, then post-conditions is operated on
+% output. If output_correctness is false, then assert will let code crash.
+if debug_flag
+    % Whether output is an array.
+    assert(isnumeric(block_indices) && numel(block_indices) >= 1);
+    % Whether each of the elements of output is a positive integer.
+    assert(all(block_indices(:) > 0) && all(mod(block_indices(:), 1) == 0));
+end
 
 end
 
