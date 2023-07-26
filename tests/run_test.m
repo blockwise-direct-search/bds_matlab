e841952a8651fc85818859a96c4a46d87fac80dd
Lht97 <lhtian97@gmail.com>
2023-07-26 22:31:28 +0800

    Test whether it works successfully

--- tests/run_test.m
+++ tests/run_test.m
@@ -1,10 +1,10 @@
 parameters.solvers_invoke = ["gsds", "sbds"];
 parameters.problems_mindim = 1;
 parameters.problems_maxdim = 5;
-parameters.sufficient_decrease_factor = [0, 1e-3];
+parameters.sufficient_decrease_factor = [0, 0];
 %parameters.powell_factor = [0, 1e-2];
 parameters.is_noisy = false;
-parameters.noise_level = 1e-5;
+parameters.noise_level = 1e-7;
 parameters.num_random = 10;
 parameters.parallel = true;
 parameters.version = "now";
