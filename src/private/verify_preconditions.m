435e14bd7e8e24e954a903670405b775e9518078
Lht97 <lhtian97@gmail.com>
2023-09-01 20:57:36 +0800

    Rewrite comments and help information of verify_preconditions.m.

--- src/private/verify_preconditions.m
+++ src/private/verify_preconditions.m
@@ -3,52 +3,75 @@ function verify_preconditions(fun, x0, options)
 %%%%%%%%%%%%%%%%%%%%%%%% precondition for fun %%%%%%%%%%%%%%%%%
 % FUN should be a function handle or a function name.
 % When detecting string, use str2func to convert.
-assert(ischarstr(fun) || isa(fun, "function_handle"));
+if ~(ischarstr(fun) && isa(str2func(fun), "function_handle"))
+    error("fun is not a function handle or a function name.");
+end
 
 %%%%%%%%%%%%%%%%%%%%%%%% precondition for x0 %%%%%%%%%%%%%%%%%
 [isrv, ~]  = isrealvector(x0);
-assert(isrv);
+if ~isrv
+    error("x0 is not a real vector.");
+end
 
 %%%%%%%%%%%%%%%%%%%%%%%% precondition for options %%%%%%%%%%%%%%%%%
 if isfield(options, "nb")
-    assert(isintegerscalar(options.nb) && options.nb>0);
+    if ~isintegerscalar(options.nb) || options.nb <= 0
+        error("options.nb is not a positive integer.");
+    end
 end
 
 if isfield(options, "maxfun_dim")
-    assert(isintegerscalar(options.maxfun_dim) && options.maxfun_dim >= 1);
+    if ~isintegerscalar(options.maxfun_dim) || options.maxfun_dim <= 0
+        error("options.maxfun_dim is not a positive integer.");
+    end
 end
 
 if isfield(options, "maxfun")
-    assert(isintegerscalar(options.maxfun) && options.maxfun >= 1);
+    if ~isintegerscalar(options.maxfun) || options.maxfun <= 0
+        error("options.maxfun is not a positive integer.");
+    end
 end
 
 if isfield(options, "alpha_all")
-    assert(isintegerscalar(options.alpha0) && options.alpha_all > 0);
+    if ~isrealscalar(options.alpha_all) || options.alpha_all <= 0
+        error("options.alpha_all is not a positive real number.");
+    end
 end
 
 if isfield(options, "expand")
-    assert(isrealscalar(options.expand) && options.expand >= 1);
+    if ~isrealscalar(options.expand) || options.expand < 1
+        error("options.expand is not a real number greater than or equal to 1.");
+    end
 end
 
 if isfield(options, "shrink")
-    assert(isrealscalar(options.shrink) && options.shrink < 1);
+    if ~isrealscalar(options.shrink) || options.shrink < 0 || options.shrink >= 1
+        error("options.shrink is not a real number in [0, 1).");
+    end
 end
 
 if isfield(options, "StepTolerance")
-    assert(isrealscalar(options.StepTolerance) && options.StepTolerance >= 0);
+    if ~isrealscalar(options.StepTolerance) || options.StepTolerance < 0
+        error("options.StepTolerance is not a real number greater than or equal to 0.");
+    end
 end
 
 if isfield(options, "sufficient_decrease_factor")
-    assert(isrealscalar(options.sufficient_decrease_factor));
-    assert(options.sufficient_decrease_factor >= 0);
+    if ~isrealscalar(options.sufficient_decrease_factor) || options.sufficient_decrease_factor < 0
+        error("options.sufficient_decrease_factor is not a real number greater than or equal to 0.");
+    end
 end
 
 if isfield(options, "polling_outer")
-    assert(ischstr(options.polling_outer));
+    if ~ischarstr(options.polling_outer)
+        error("options.polling_outer is not a string.");
+    end
 end
 
 if isfield(options, "polling_inner")
-    assert(ischstr(options.polling_inner));
+    if ~ischarstr(options.polling_inner)
+        error("options.polling_inner is not a string.");
+    end
 end
 
 end
