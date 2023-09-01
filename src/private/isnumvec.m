5c71bd4921e900e309edd5c461b1d21800b766ac
Lht97 <lhtian97@gmail.com>
2023-09-01 20:43:14 +0800

    Rewrite comments and help information of verify_postconditions.m.

--- src/private/isnumvec.m
+++ src/private/isnumvec.m
@@ -1,4 +1,5 @@
 function is_numvec = isnumvec(x)
 % Verify whether x is a number vector.
+
 is_numvec = isnumeric(x) && isvector(x);
 end
--- src/private/verify_postconditions.m
+++ src/private/verify_postconditions.m
@@ -1,35 +1,55 @@
 function verify_postconditions(fun, xval, fval, exitflag, output)
 
-% Verify whether xval is a real column or scalar
-assert(isnumeric(xval) && isreal(xval) && iscolumn(xval));
+% Verify whether xval is a real column or scalar.
+if ~(isnumeric(xval) && isreal(xval) && iscolumn(xval))
+    error("xval is not a real column or scalar.");
+end
 
-% Verify whether fval is a real number
-assert(isrealscalar(fval));
+% Verify whether fval is a real number.
+if ~(isrealscalar(fval))
+    error("fval is not a real number.");
+end
 
-% Verify whether exitflag is an integer
-assert(isintegerscalar(exitflag));
+% Verify whether exitflag is an integer.
+if ~(isintegerscalar(exitflag))
+    error("exitflag is not an integer.");
+end
 
-% Verify whether nf is a postive integer
-assert(isfield(output, "funcCount"));
+% Verify whether nf is a postive integer.
+if ~isfield(output, "funcCount")
+    error("output.funcCount does not exist.");
+end
 nf = output.funcCount;
-assert(isintegerscalar(nf) && nf > 0);
+if ~(isintegerscalar(nf) && nf > 0)
+    error("output.funcCount is not a positive integer.");
+end
 
-% Verify whether output is a structure
-assert(isa(output, "struct"));
+% Verify whether output is a structure.
+if ~(isstruct(output))
+    error("output is not a structure.");
+end
 
-% Verify whether output.fhist exists
-assert(isfield(output, "fhist"));
+% Verify whether output.fhist exists.
+if ~isfield(output, "fhist")
+    error("output.fhist does not exist.");
+end
 fhist = output.fhist;
 nhist = length(fhist);
 
-% Verify whether output.xhist exists
-assert(isfield(output, "xhist"));
+% Verify whether output.xhist exists.
+if ~isfield(output, "xhist")
+    error("output.xhist does not exist.");
+end
 xhist = output.xhist;
-% Verify whether xhist is a real matrix of size
-assert(isrealmatrix(xhist) && any(size(xhist) == [length(xval), nhist]));
+% Verify whether xhist is a real matrix of size.
+if ~(isrealmatrix(xhist) && any(size(xhist) == [length(xval), nhist]))
+    error("output.xhist is not a real matrix.");
+end
 
-% Check whether length(fhist) is equal to length(xhist) and nf
-assert(size(xhist, 2) == nf && length(fhist) == nf);
+% Check whether length(fhist) is equal to length(xhist) and nf respectively.
+if ~(length(fhist) == length(xhist) && size(xhist, 2) == nf)
+    error("length of fhist is not equal to length of xhist or nf.");
+end
 
 % Check whether fhist = fun(xhist)
 % TODO: there is a way to avoid using loop, try to find it.
