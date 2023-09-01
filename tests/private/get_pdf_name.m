d5f6f035aa030bbbc9a54fe2ef3bc4750abc34b3
Lht97 <lhtian97@gmail.com>
2023-09-01 23:35:23 +0800

    Rewrite comments and help information of get_pdf_name.m.

--- tests/private/get_pdf_name.m
+++ tests/private/get_pdf_name.m
@@ -1,21 +1,25 @@
 function [pdfname] = get_pdf_name(parameters, i)
-% Get the part of pdfname for the i-th solver respectively.
+% GET_PDF_NAME gets the part of pdfname of the i-th solver.
 prima_list = ["cobyla", "uobyqa", "newuoa", "bobyqa", "lincoa"];
 
 if parameters.solvers_invoke(i) == "bds"
     pdfname = parameters.Algorithm(i);
+
 elseif parameters.solvers_invoke(i) == "bds_powell"
     powell_factor_stamp = int2str(int32(-log10(parameters.powell_factor(i))));
     pdfname = strcat("CBDS_Powell", "_", powell_factor_stamp);
+
 elseif parameters.solvers_invoke(i) == "bds_cunxin"
     cunxin_factor_tmp = parameters.cunxin_factor{i};
     cunxin_factor_length = length(cunxin_factor_tmp);
     cunxin_factor_stamp = [];
+
     for j = 1:cunxin_factor_length
         cunxin_factor_stamp = strcat(cunxin_factor_stamp, "_", ... 
             int2str(int32(-log10(cunxin_factor_tmp(j)))));
     end
     pdfname = strcat("CBDS_Cunxin", "_", cunxin_factor_stamp);
+
 elseif any(strcmpi(prima_list, parameters.solvers_invoke(i)))
         pdfname = parameters.solvers_invoke(i);
         if isfield(parameters, "version")
@@ -23,12 +27,16 @@ elseif any(strcmpi(prima_list, parameters.solvers_invoke(i)))
                 pdfname = strcat(parameters.solvers_invoke(i), "_", "classical");
             end
         end
+
 elseif parameters.solvers_invoke(i) == "matlab_fminsearch"
     pdfname = strcat("fminsearch", "_", "simplex");
+
 elseif parameters.solvers_invoke(i) == "matlab_patternsearch"
     pdfname = strcat("patternsearch", "_", "gps");
+
 elseif parameters.solvers_invoke(i) == "matlab_fminunc"
     pdfname = strcat("fminunc", "_", parameters.fminunc_type);
+    
 end
 
 end
