dd9a29c0f6d51e4b14dfeb6ad2b5d46ccbe013dc
Lht97 <lhtian97@gmail.com>
2023-11-02 00:10:02 +0800

    return the nlopt_wrapper and rosenbrock_example to the previous version

--- examples/rosenbrock_example.m
+++ examples/rosenbrock_example.m
@@ -1,46 +1,36 @@
 function rosenbrock_example()
 %This file is cited from https://github.com/libprima/prima/blob/main/matlab/examples/rosenbrock_example.m, which is
-%written by Zaikun Zhang. 
+%written by Zaikun Zhang.
 %ROSENBROCK_EXAMPLE illustrates how to use bds.
 %
 %   ***********************************************************************
-%   Authors:    Haitian Li (haitian-li@connect.polyu.hk) 
+%   Authors:    Haitian Li (haitian-li@connect.polyu.hk)
 %               and Zaikun ZHANG (zaikun.zhang@polyu.edu.hk)
 %               Department of Applied Mathematics,
 %               The Hong Kong Polytechnic University
 %
 %   ***********************************************************************
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Attribute: public (can be called directly by users)
-% 
+%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 x0 = [0; 0; 0];  % starting point
 
-% options.maxfun = 1e4;
-% options.StepTolerance = eps;
-% options.Algorithm = "cbds";
-options.Algorithm = "newuoa";
+options.maxfun = 1e4;
+options.StepTolerance = eps;
+options.Algorithm = "cbds";
 
 fullpath = mfilename("fullpath");
 path_examples = fileparts(fullpath);
 path_bds = fileparts(path_examples);
 path_src = fullfile(path_bds, "src");
-path_competitors = fullfile(path_bds, "tests", "competitors");
 addpath(path_src)
-addpath(path_competitors)
 
 % The following syntax is identical to fmincon:
-% [xval, fval, exitflag, output] = nlopt_wrapper(@chrosen, x0, options)
-[xopt, fmin, retcode] = nlopt_wrapper(@chrosen, x0, options)
-
+[xval, fval, exitflag, output] = bds(@chrosen, x0, options)
 rmpath(path_src)
-rmpath(path_competitors)
-
 return
-
-
 function f = chrosen(x)  % the subroutine defining the objective function
 f = sum((x(1:end-1)-1).^2 + 4*(x(2:end)-x(1:end-1).^2).^2);
 return
--- tests/competitors/nlopt_wrapper.m
+++ tests/competitors/nlopt_wrapper.m
@@ -1,33 +1,28 @@
-function [xopt, fmin, retcode] = nlopt_wrapper(fun, x0, options)
-%A wrapper for NLopt, which is a library for nonlinear local and global optimization, 
+function nlopt_wrapper(fun, x0, options)
+%A wrapper for NLopt, which is a library for nonlinear local and global optimization,
 %for functions with and without gradient information. For more information, please
 %see https://github.com/stevengj/nlopt.
 %
-
 % Dimension
 n = numel(x0);
-
 if isfield(options, "Algorithm")
     switch options.Algorithm
         case "cobyla"
-         opt.algorithm = NLOPT_LN_COBYLA; 
+            opt.algorithm = NLOPT_LN_COBYLA;
         case "newuoa"
-         opt.algorithm = NLOPT_LN_NEWUOA;
+            opt.algorithm = NLOPT_LN_NEWUOA;
         case "bobyqa"
-         opt.algorithm = NLOPT_LN_BOBYQA;
+            opt.algorithm = NLOPT_LN_BOBYQA;
     end
 else
     opt.algorithm = NLOPT_LN_NEWUOA;
 end
-
 opt.min_objective = fun;
-
 if isfield(options, "stopval")
     opt.stopval = options.stopval;
 else
     opt.stopval = get_default_constant("ftarget");
 end
-
 if isfield(options, "StepTolerance")
     opt.ftol_rel = options.StepTolerance;
     opt.ftol_abs = options.StepTolerance;
@@ -35,15 +30,11 @@ else
     opt.ftol_rel = eps;
     opt.ftol_abs = eps;
 end
-
 if isfield(options, 'maxtime')
     opt.maxtime = options.maxtime;
 else
     opt.maxtime = 0;
 end
-
-opt.initial_step = [2 1 1]; 
-
 % Set MAXFUN to the maximum number of function evaluations.
 if isfield(options, "maxfun_factor") && isfield(options, "maxfun")
     opt.maxeval = min(options.maxfun_factor*n, options.maxfun);
@@ -54,9 +45,7 @@ elseif isfield(options, "maxfun")
 else
     opt.maxeval = min(get_default_constant("maxfun"), get_default_constant("maxfun_factor")*n);
 end
-
 %disp("nlopt is invoked");
-[xopt, fmin, retcode] = nlopt_optimize(opt, x0');
-%nlopt_optimize(opt, x0');
+nlopt_optimize(opt, x0');
 
 end
