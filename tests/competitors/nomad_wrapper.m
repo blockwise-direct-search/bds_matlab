e32659c2fbfc5a1de783b72be999885a4700ff78
Lht97 <lhtian97@gmail.com>
2024-12-14 13:17:15 +0800

    Check spellings

--- tests/competitors/nomad_wrapper.m
+++ tests/competitors/nomad_wrapper.m
@@ -21,7 +21,7 @@ params = struct('MAX_BB_EVAL', num2str(MaxFunctionEvaluations), 'max_eval',num2s
 % As of NOMAD version 4.4.0 and OptiProfiler commit 24d8cc0, the following line is 
 % necessary. Otherwise, NOMAD will throw an error, complaining that the blackbox 
 % evaluation fails. This seems to be because OptiProfiler wraps the function 
-% handle in a way that NOMAD does not expect: NONAD expects a function handle 
+% handle in a way that NOMAD does not expect: NOMAD expects a function handle 
 % `fun` with the signature fun(x), where x is a column vector, while OptiProfiler 
 % produces one with the signature @(varargin)featured_problem.fun(varargin{:}).
 fun = @(x) fun(x(:));
